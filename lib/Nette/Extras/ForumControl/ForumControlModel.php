e8d8b3fa5b3e0c92368a0df3a559fa89e9c0e6ee
mm-marek <marek@mixa.name>
2011-09-13 15:28:53 +0300

    changed dibi to NotORM

--- lib/Nette/Extras/ForumControl/ForumControlModel.php
+++ lib/Nette/Extras/ForumControl/ForumControlModel.php
@@ -16,45 +16,14 @@
 
  class ForumControlModel extends Object implements IForumControlModel
  {
-   /**
-    * Forum ID
-    *
-    * @access protected
-    * @var int
-    * @since 1.0.0
-    */
    protected $forumId;
 
-   /**
-    * Instance of database connection
-    *
-    * @access protected
-    * @var DibiConnection
-    * @since 1.0.0
-    */
    protected $connection;
 
-   /**#@+
-    * Database tables
-    *
-    * @access protected
-    * @var string
-    * @since 1.0.0
-    */
    protected $tForum;
    protected $tThreads;
-   /**#@- */
 
-   /**
-    * Initialization
-    *
-    * @access public
-    * @param int $forumId forum ID
-    * @param DibiConnection $connection instance of database connection
-    * @return void
-    * @since 1.0.0
-    */
-   public function __construct($forumId, \DibiConnection $connection)
+   public function __construct($forumId, \NotORM $connection)
    {
      $this->forumId = (int) $forumId;
      $this->connection = $connection;
@@ -63,185 +32,111 @@
      $this->tThreads = 'forum_threads';
    }
 
-   /**
-    * Returns count of topics
-    *
-    * @access public
-    * @return int
-    * @since 1.0.0
-    */
-   public function getCount()
-   {
-     return (int) $this->connection->select('COUNT([id_thread])')
-                                   ->from($this->tThreads)
-                                   ->where('[id_forum] = %i', $this->forumId)
-                                   ->fetchSingle();
-   }
+    public function getCount()
+    {
+        $tableName = $this->tThreads;
+        return (int) $this->connection->$tableName(array(
+                    "id_forum" => $this->forumId
+                ))->count("*");
+    }
 
-   /**
-    * Returns forum title
-    *
-    * @access public
-    * @return string
-    * @since 1.0.0
-    */
-   public function getTitle()
-   {
-     return $this->connection->select('forum')
-                             ->from($this->tForum)
-                             ->where('[id_forum] = %i', $this->forumId)
-                             ->fetchSingle();
-   }
+    public function getTitle()
+    {
+        $tableName = $this->tForum;
+        return $this->connection->$tableName(array(
+                    "id_forum" => $this->forumId
+                ))->select('forum')->fetch();
+    }
 
-   /**
-    * Returns topic
-    *
-    * @access public
-    * @param int $topicId topic ID
-    * @return DibiRow
-    * @since 1.0.0
-    */
-   public function getTopic($topicId)
-   {
-     return $this->connection->select('*')
-                             ->from($this->tThreads)
-                             ->where('[id_forum] = %i', $this->forumId)
-                               ->and('[id_thread] = %i', $topicId)
-                             ->fetch();
-   }
+    public function getTopic($topicId)
+    {
+        $tableName = $this->tThreads;
+        return $this->connection->$tableName(array(
+                "id_forum" => $this->forumId, 
+                "id_thread" => $topicId
+            ))->select('*')->fetch();
+    }
 
-   /**
-    * Returns selected topics
-    *
-    * @access public
-    * @param array $topicIds array of topic ID's
-    * @return array
-    * @since 1.0.0
-    */
-   public function getTopics(array $topicIds)
-   {
-     $topicIds = implode(', ', $topicIds);
+    public function getTopics(array $topicIds)
+    {
+        $tableName = $this->tThreads;
 
-     return $this->connection->select('*')
-                             ->from($this->tThreads)
-                             ->where('[id_forum] = %i', $this->forumId)
-                               ->and('[id_thread] IN (%sql)', $topicIds)
-                             ->orderBy('[sequence]', \dibi::ASC)
-                             ->fetchAll();
-   }
+        return $this->connection->$tableName(array(
+                "id_forum" => $this->forumId, 
+                "id_thread" => $topicIds
+            ))>select('*')
+            ->order('sequence ASC');
+    }
 
-   /**
-    * Returns threads
-    *
-    * @access public
-    * @return array
-    * @since 1.0.0
-    */
-   public function getThreads()
-   {
-     return $this->connection->select(array(
-                                 'id_thread',
-                                 'depth',
-                                 'name',
-                                 'title',
-                                 'topic',
-                                 'date_time')
-                               )
-                             ->select('DATE_FORMAT([date_time], %s)', '%e. %c. %Y, %H:%i')->as('cz_date_time')
-                             ->from($this->tThreads)
-                             ->where('[id_forum] = %i', $this->forumId)
-                             ->orderBy('[sequence]', \dibi::ASC)
-                             ->fetchAll();
-   }
+    public function getThreads()
+    {
+        $tableName = $this->tThreads;
+        $rows = $this->connection->$tableName(array(
+                    "id_forum" => $this->forumId
+                ))
+                ->select(
+                 'id_thread, depth, name, title, topic, date_time, DATE_FORMAT(date_time, \'%e. %c. %Y, %H:%i\') AS cz_date_time')
+               ->order('sequence ASC');
+        return $rows;
+    }
 
-   /**
-    * Inserts topic
-    *
-    * @access public
-    * @param \Nette\ArrayHash $data data
-    * @param int $topicId topic ID to reply
-    * @return void
-    * @uses getTopic()
-    * @since 1.0.0
-    */
-   public function insert(\Nette\ArrayHash $data, $topicId)
-   {
-     $data->id_forum = $this->forumId;
+    public function insert(\Nette\ArrayHash $data, $topicId)
+    {
+        $tableName = $this->tThreads;
+        
+        $data->id_forum = $this->forumId;
 
-     $this->connection->query('LOCK TABLES ['.$this->tThreads.'] WRITE');
+        $this->connection->query('LOCK TABLES ['.$this->tThreads.'] WRITE');
 
-     $re = $this->getTopic($topicId);
+        $re = $this->getTopic($topicId);
+        
+        if ($topicId && $re !== FALSE)
+        {
+            $re = $this->connection->$tableName('id_forum', $this->forumId)
+                    ->select('MIN(sequence) - 1 AS new_sequence')
+                    ->select($re['depth'] . ' + 1 AS new_depth')
+                    ->where(new \NotORM_Literal('sequence > ' . $re['sequence']))
+                    ->where(new \NotORM_Literal('depth <= ' . $re['depth']))
+                    ->fetch();
+            
+            //\Nette\Diagnostics\Debugger::barDump($re['new_sequence']);
 
-     if ($topicId && $re !== FALSE)
-     {
-       $re = $this->connection->select('MIN([sequence]) - 1')->as('new_sequence')
-                              ->select('%i + %i', $re->depth, 1)->as('new_depth')
-                              ->from($this->tThreads)
-                              ->where('[id_forum] = %i', $this->forumId)
-                                ->and('[sequence] > %i', $re->sequence)
-                                ->and('[depth] <= %i', $re->depth)
-                              ->fetch();
+            if ($re['new_sequence'])
+            {
+                $this->connection->$tableName('id_forum', $this->forumId)
+                        ->where(new \NotORM_Literal('sequence > ' . $re['new_sequence']))
+                        ->update(array('sequence' => new \NotORM_Literal('sequence + 1')));
+            }
+            else
+            {
+                $re = $this->connection->$tableName("id_forum", $this->forumId)
+                        ->select('MAX(sequence) AS new_sequence')
+                        ->select($re['new_depth'] . ' AS new_depth')
+                        ->fetch();
+            }
+            
+        }
+        else
+        {
+            $re = $this->connection->$tableName("id_forum", $this->forumId)
+                        ->select('MAX(sequence) AS new_sequence')
+                        ->select('0 AS new_depth')
+                        ->fetch();
+        }
 
-       if ($re->new_sequence)
-       {
-         $this->connection->query('UPDATE ['.$this->tThreads.'] SET
-                                     [sequence] = [sequence] + %i
-                                   WHERE [id_forum] = %i
-                                     AND [sequence] > %i',
-                                   1, $this->forumId, $re->new_sequence);
-       }
-       else
-       {
-         $re = $this->connection->select('MAX([sequence])')->as('new_sequence')
-                                ->select('%i', $re->new_depth)->as('new_depth')
-                                ->from($this->tThreads)
-                                ->where('[id_forum] = %i', $this->forumId)
-                                ->fetch();
-       }
-     }
-     else
-     {
-       $re = $this->connection->select('MAX([sequence])')->as('new_sequence')
-                              ->select('%i', 0)->as('new_depth')
-                              ->from($this->tThreads)
-                              ->where('[id_forum] = %i', $this->forumId)
-                              ->fetch();
-     }
+        $data->sequence = $re['new_sequence'] + 1;
+        $data->depth = $re['new_depth'];
 
-     $data->sequence = $re->new_sequence + 1;
-     $data->depth = $re->new_depth;
+        $this->connection->$tableName()->insert($data);
+        $this->connection->query('UNLOCK TABLES');
+    }
 
-     $this->connection->insert($this->tThreads, $data)->execute();
-     $this->connection->query('UNLOCK TABLES');
-   }
+    public function existsTopic($topicId)
+    {
+        $tableName = $this->tThreads;
+        return (bool) $this->connection->$tableName(array('id_forum' => $this->forumId, 'id_thread' => $topicId))->count('*');
+    }
 
-   /**
-    * Exists topic?
-    *
-    * @access public
-    * @param int $topicId topic ID
-    * @return bool
-    * @since 1.0.0
-    */
-   public function existsTopic($topicId)
-   {
-     return (bool) $this->connection->select('COUNT([id_thread])')
-                                    ->from($this->tThreads)
-                                    ->where('[id_forum] = %i', $this->forumId)
-                                      ->and('[id_thread] = %i', $topicId)
-                                    ->fetchSingle();
-   }
-
-   /**
-    * Gets relative time
-    *
-    * @author Jakub Vrána
-    * @access public
-    * @param mixed $time time
-    * @return string
-    * @static
-    * @since 1.0.0
-    */
    public static function timeAgoInWords($time)
    {
      if (!$time)
@@ -257,36 +152,36 @@
      $delta = round($delta / 60);
 
      if ($delta == 0)
-       return 'a while ago';
+       return 'před chvilkou';
 
      if ($delta == 1)
-       return 'a minute ago';
+       return 'před minutou';
 
      if ($delta < 45)
-       return $delta.' minutes ago';
+       return 'před' . $delta . ' minutami';
 
      if ($delta < 90)
-       return 'an hour ago';
+       return 'před hodinou';
 
      if ($delta < 1440)
-       return round($delta / 60).' hours ago';
+       return 'před' . round($delta / 60).' hodinami';
 
      if ($delta < 2880)
-       return 'yesterday';
+       return 'včera';
 
      if ($delta < 43200)
-       return round($delta / 1440).' days ago';
+       return 'před' . round($delta / 1440).' dny';
 
      if ($delta < 86400)
-       return 'last month';
+       return 'minulý měsíc';
 
      if ($delta < 525960)
-       return round($delta / 43200).' months ago';
+       return 'před' . round($delta / 43200).' měsíci';
 
      if ($delta < 1051920)
-       return 'last year';
+       return 'minulý rok';
 
-     return round($delta / 525960).' years lety';
+     return 'před' . round($delta / 525960).' lety';
    }
  }
 ?>
